b7d03127179215fad11d56e0a493293c16e0793c
robinhwang <binbinhuangcsu@gmail.com>
2013-01-19 21:05:39 +0800

    15

---
+++ 15_exceptions.rb
@@ -0,0 +1,62 @@
+class AboutExceptions < EdgeCase::Koan
+  class MySpecialError < RuntimeError
+  end
+
+  def test_exceptions_inherit_from_Exception
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
+  end
+
+  def test_rescue_clause
+    result = nil
+    begin
+      fail "Oops"
+      #fail is alias of raise
+    rescue StandardError => ex
+      result = :exception_handled
+    end
+
+    assert_equal :exception_handled, result
+    assert_equal true, ex.is_a?(StandardError), "Should be a Standard Error"
+    assert_equal true, ex.is_a?(RuntimeError), "Should be a RuntimeError"
+
+    assert RuntimeError.ancestors.include?(StandardError),
+      "RuntimeError is a subclass of StandardError"
+
+    assert_equal "Oops", ex.message
+  end
+
+  def test_raising_a_particular_error
+    result = nil
+    begin
+      #'raise' and 'fail' are synonyms
+      raise MySpecialError, "My Message"
+    rescue MySpecialError => ex
+      result = :exception_handled
+    end
+
+    assert_equal :exception_handled, result
+    assert_equal "My Message", ex.message
+  end
+
+  def test_ensure_clause
+    result = nil
+    begin
+      fail "Oops"
+    rescue StandardError => ex
+      #
+    ensure
+      result = :always_run
+    end
+
+    assert_equal :always_run, result
+  end
+
+  def test_asserting_an_error_is_raised
+    assert_raise(MySpecialError) do
+      raise MySpecialError.new("New instances can be raised directly")
+    end
+  end
+end
